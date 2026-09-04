7a8f3882bdbfd726a2cf10fb22529d93b2cee0a9
Iulian Timofei <iuliantim21@gmail.com>
2026-09-04 13:02:47 +0200

    test: add Testcontainers integration test base class (PFR-27)

--- src/test/java/team3/dev/restaurante/RestauranteApplicationTests.java
+++ src/test/java/team3/dev/restaurante/RestauranteApplicationTests.java
@@ -1,13 +1,28 @@
 package team3.dev.restaurante;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
-class RestauranteApplicationTests {
+import java.sql.Connection;
+
+import javax.sql.DataSource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+class RestauranteApplicationTests extends AbstractIntegrationTest {
+
+	@Autowired
+	DataSource dataSource;
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	void connectsToRealPostgres() throws Exception {
+		try (Connection connection = dataSource.getConnection()) {
+			String product = connection.getMetaData().getDatabaseProductName();
+			assertThat(product).isEqualTo("PostgreSQL");
+		}
+	}
 }
