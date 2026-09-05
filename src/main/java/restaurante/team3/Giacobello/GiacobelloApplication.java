6f82d444b462b80ae6bde17eb9cc1bdf0703fb82
Rubén Campal <ruben.campal.lopez1996@gmail.com>
2026-09-05 12:07:55 +0200

    Initial commit

---
+++ src/main/java/restaurante/team3/Giacobello/GiacobelloApplication.java
@@ -0,0 +1,13 @@
+package restaurante.team3.Giacobello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiacobelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiacobelloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/restaurante/team3/Giacobello/GiacobelloApplicationTests.java
@@ -0,0 +1,13 @@
+package restaurante.team3.Giacobello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiacobelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
