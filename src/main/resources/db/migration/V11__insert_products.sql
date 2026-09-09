667c3f316e257e706bad1226e4bc5428b7fcbf12
Rubén Campal <ruben.campal.lopez1996@gmail.com>
2026-09-09 15:08:06 +0200

    refactor: add image_url in product insert migration

--- src/main/resources/db/migration/V11__insert_products.sql
+++ src/main/resources/db/migration/V11__insert_products.sql
@@ -2,77 +2,90 @@ INSERT INTO products (
     name,
     description,
     category_id,
-    price
+    price,
+    image_url
 ) VALUES 
 (
     'Pasta alla Singleton',
     'Pasta con salsa de tomate, berenjena frita, albahaca y ricotta salata, todo un clásico',
     1,
-    12.50
+    12.50,
+    '/images/products/Pasta_alla_Singleton.jpg'
 ),
 (
     'Arancini Gitilini',
     'Crujientes bolas de arroz rellenas de ragú, mozzarella y guisantes, empanadas y versionadas con convencional commits',
     1,
-    9.50
+    9.50,
+    '/images/products/Arancini_Gitilini.jpg'
 ),
 (
-    'Lasagna al aquitectun',
+    'Lasagna al arquitectum',
     ' Capas de pasta fresca con ragú de carne, bechamel, tomate y parmesano. Todo perfectamente atomizado para que el tomate no conozca nuestro modelo de negocio',
     1,
-    14.50
+    14.50,
+    '/images/products/Lasagna_al_arquitecum.jpg'
 ),
 (
-    'Parmigiana di Dockerananze',
+    'Parmigiana di Dockernanze',
     'Capas de berenjena, salsa de tomate, mozzarella, albahaca y parmesano, horneadas en un contenedor para no chamuscar la cocina',
     1,
-    13
+    13,
+    '/images/products/Parmigiana_di_Dockernanze.jpg'
 ),
 (
     'Tiramisú di Giaco',
     'Nuestro postre estrella: tiramisú artesanal con mascarpone, café y cacao. Viene con ejercicio de regalo',
     2,
-    7.50
+    7.50,
+    '/images/products/Tiramisu_di_Giaco.jpg'
 ),
 (
     'Panna Cotta Commit',
     'Panna Cotta italiana de vainilla con frutos rojos, lista y aprobada para servir',
     2,
-    6
+    6,
+    '/images/products/Panna_Cotta_Commit.jpg'
 ),
 (
     'Cassata di Merge',
     'Tradicional cassata siciliana con bizcocho, ricotta, frutas confitadas y mazapán. Una combinación que funciona... la mayoría de las veces',
     2,
-    6
+    6,
+    '/images/products/Cassata_di_Merge.jpg'
 ),
 (
     'AI a la Carte',
     'El más vendido. No lo hemos hecho ni nosotros pero no paran de comerlo',
     2,
-    8
+    8,
+    '/images/products/AI_a_la_Carte.jpg'
 ),
 (
     'Limonata Siciliana',
     'Refrescante limonada artesanal pero con un nombre más molón',
     3,
-    4.50
+    4.50,
+    '/images/products/Limonata_Siciliana.jpg'
 ),
 (
     'Acqua di localhost',
     'Lágrimas acumuladas de alumnos de Giaco que no pudieron terminar los ejercicios',
     3,
-    60
+    60,
+    '/images/products/Acqua_di_localhost.jpg'
 ),
 (
-    'Giacomo-cola',
+    'Giaco-cola',
     'Refresco de cola artesanal con una receta tan bien encapsulada que ni el propio Giacomo puede acceder a ella',
     3,
-    4
+    4,
+    '/images/products/Giaco_cola.jpg'
 ),
 (
     'Divergenza di Git',
     'Refrescante cóctel elaborado a partir de dos ramas divergentes. Recomendado si entras a nuestro repositorio',
     3,
-    8.50
+    8.50,
+    '/images/products/Divergenza_di_Git.jpg'
 );
