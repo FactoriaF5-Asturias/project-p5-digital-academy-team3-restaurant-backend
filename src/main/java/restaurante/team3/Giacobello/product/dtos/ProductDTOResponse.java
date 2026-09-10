3cda90475c8366319a645f22df623ee08ef83d3b
Iulian Timofei <iuliantim21@gmail.com>
2026-09-10 09:44:17 +0200

    fix. added category to ProductDTO

---
+++ src/main/java/restaurante/team3/Giacobello/product/dtos/ProductDTOResponse.java
@@ -0,0 +1,45 @@
+package restaurante.team3.Giacobello.product.dtos;
+
+import java.math.BigDecimal;
+
+public class ProductDTOResponse {
+
+    private Long id;
+    private String name;
+    private String description;
+    private String category;
+    private BigDecimal price;
+    private Boolean status;
+    private String imageUrl;
+
+    public ProductDTOResponse() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+}
