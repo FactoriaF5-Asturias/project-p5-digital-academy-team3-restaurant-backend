558de9d61c6b416e62c5ee7ca3a7f6e0f1e123fa
Iulian Timofei <iuliantim21@gmail.com>
2026-09-10 09:32:26 +0200

    feat add ProductRepository

---
+++ src/main/java/restaurante/team3/Giacobello/product/ProductRepository.java
@@ -0,0 +1,7 @@
+package restaurante.team3.Giacobello.product;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
+
+}
