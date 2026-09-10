86f11f7f0e3a751008607e335ac5bcd9be53580e
Iulian Timofei <iuliantim21@gmail.com>
2026-09-10 14:52:07 +0200

    feat: add ProductMapper and response DTO

--- src/main/java/restaurante/team3/Giacobello/product/dtos/ProductDTOResponse.java
+++ src/main/java/restaurante/team3/Giacobello/product/dtos/ProductDTOResponse.java
@@ -2,44 +2,14 @@ package restaurante.team3.Giacobello.product.dtos;
 
 import java.math.BigDecimal;
 
-public class ProductDTOResponse {
+public record ProductDTOResponse(
 
-    private Integer id;
-    private String name;
-    private String description;
-    private String category;
-    private BigDecimal price;
-    private Boolean status;
-    private String imageUrl;
+    Integer id,
+    String name,
+    String description,
+    String category,
+    BigDecimal price,
+    Boolean status,
+    String imageUrl) {
 
-    public ProductDTOResponse() {
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public BigDecimal getPrice() {
-        return price;
-    }
-
-    public Boolean getStatus() {
-        return status;
-    }
-
-    public String getImageUrl() {
-        return imageUrl;
-    }
 }
