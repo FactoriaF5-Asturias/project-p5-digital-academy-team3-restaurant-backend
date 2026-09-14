74fc94df9c3ae9cb98967b486268afd0fcb5ed88
Rubén Campal <ruben.campal.lopez1996@gmail.com>
2026-09-14 09:21:12 +0200

    fix: update API endpoint mapping to use configurable property

--- src/main/java/restaurante/team3/Giacobello/orders/controller/OrderController.java
+++ src/main/java/restaurante/team3/Giacobello/orders/controller/OrderController.java
@@ -11,7 +11,7 @@ import restaurante.team3.Giacobello.orders.service.OrderService;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/orders")
+@RequestMapping("${api-endpoint}/orders")
 public class OrderController {
 
     private final OrderService orderService;
