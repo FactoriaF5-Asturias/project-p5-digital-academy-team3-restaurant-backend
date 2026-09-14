245b51000820fd05d0f707579ee6aa006ad9ef03
Ekaterina Zotova <139338863+zookatt@users.noreply.github.com>
2026-09-14 15:19:48 +0200

    feat: add fields orderTypeName y paymentMethodName to OrderDTOResponse

--- src/main/java/restaurante/team3/Giacobello/orders/dto/OrderDTOResponse.java
+++ src/main/java/restaurante/team3/Giacobello/orders/dto/OrderDTOResponse.java
@@ -3,9 +3,12 @@ package restaurante.team3.Giacobello.orders.dto;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-public record OrderDTOResponse(Integer id,
-        Integer tabletId,
-        String statusName,
-        BigDecimal totalAmount,
-        LocalDateTime createdAt) {
+public record OrderDTOResponse(
+                Integer id,
+                Integer tabletId,
+                String orderTypeName,
+                String paymentMethodName,
+                String statusName,
+                BigDecimal totalAmount,
+                LocalDateTime createdAt) {
 }
