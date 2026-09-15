c82667b5148043ddd5d3576f010caf41c5e08cb8
Iulian Timofei <iuliantim21@gmail.com>
2026-09-15 14:44:09 +0200

    test: update CategoryServiceImpl test for new constructor

--- src/test/java/restaurante/team3/Giacobello/categories/service/CategoryServiceImplTest.java
+++ src/test/java/restaurante/team3/Giacobello/categories/service/CategoryServiceImplTest.java
@@ -9,6 +9,7 @@ import restaurante.team3.Giacobello.categories.dto.CategoryDTOResponse;
 import restaurante.team3.Giacobello.categories.entity.CategoryEntity;
 import restaurante.team3.Giacobello.categories.mappers.CategoryMapper;
 import restaurante.team3.Giacobello.categories.repository.CategoryRepository;
+import restaurante.team3.Giacobello.product.repository.ProductRepository;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
@@ -19,7 +20,8 @@ class CategoryServiceImplTest {
     void findAllReturnsCategories() {
         CategoryRepository repository = mock(CategoryRepository.class);
         CategoryMapper mapper = mock(CategoryMapper.class);
-        CategoryServiceImpl service = new CategoryServiceImpl(repository, mapper);
+        ProductRepository productRepository = mock(ProductRepository.class);
+        CategoryServiceImpl service = new CategoryServiceImpl(repository, mapper, productRepository);
 
         CategoryEntity category = new CategoryEntity(1, "Pizzas");
         CategoryDTOResponse dto = new CategoryDTOResponse(1, "Pizzas");
