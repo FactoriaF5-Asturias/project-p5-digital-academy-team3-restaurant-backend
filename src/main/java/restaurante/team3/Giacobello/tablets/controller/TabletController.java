6efd3fe822d6c2a9d7b49bef53fb0da2a79cb235
Rubén Campal <ruben.campal.lopez1996@gmail.com>
2026-09-15 20:13:44 +0200

    feat: add findById endpoint to TabletController for retrieving tablet by ID

--- src/main/java/restaurante/team3/Giacobello/tablets/controller/TabletController.java
+++ src/main/java/restaurante/team3/Giacobello/tablets/controller/TabletController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +23,10 @@ public class TabletController {
     @GetMapping 
     public ResponseEntity<List<TabletDTOResponse>> findAll() {
         return ResponseEntity.ok(tabletService.findAll());
-    }    
+    }  
+    
+    @GetMapping ("/{id}")
+    public ResponseEntity<TabletDTOResponse> findById(@PathVariable Integer id) {
+        return ResponseEntity.ok(tabletService.findById(id));
+    }
 }
