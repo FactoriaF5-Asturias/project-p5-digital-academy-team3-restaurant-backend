65dc3b04a287c8d2966932016aabf6c926ffccb5
Rubén Campal <ruben.campal.lopez1996@gmail.com>
2026-09-15 19:44:06 +0200

    feat: implement TabletService and TabletServiceImpl for tablet management

---
+++ src/main/java/restaurante/team3/Giacobello/tablets/service/TabletServiceImpl.java
@@ -0,0 +1,33 @@
+package restaurante.team3.Giacobello.tablets.service;
+
+import java.util.List;
+
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import org.springframework.transaction.annotation.Transactional;
+import restaurante.team3.Giacobello.tablets.dtos.TabletDTOResponse;
+import restaurante.team3.Giacobello.tablets.mappers.TabletMapper;
+import restaurante.team3.Giacobello.tablets.repository.TabletRepository;
+
+@Service 
+public class TabletServiceImpl implements TabletService {
+    private final TabletRepository tabletRepository;
+    private final TabletMapper tabletMapper;
+
+    public TabletServiceImpl(TabletRepository tabletRepository, TabletMapper tabletMapper) {
+        this.tabletRepository = tabletRepository;
+        this.tabletMapper = tabletMapper;
+    }
+
+    @Override 
+    @Transactional(readOnly = true)
+    public List<TabletDTOResponse> findAll() {
+        return tabletRepository.findAll(Sort.by("name").ascending())
+            .stream()
+            .map(tabletMapper::toResponse)
+            .toList();
+    }
+
+    
+}
