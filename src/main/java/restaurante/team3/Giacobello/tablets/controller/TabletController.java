b5d6bfcb412d7c65424ee317aef724f5f00d367f
Rubén Campal <ruben.campal.lopez1996@gmail.com>
2026-09-15 19:48:07 +0200

    feat: add TabletController for managing tablet endpoints

---
+++ src/main/java/restaurante/team3/Giacobello/tablets/controller/TabletController.java
@@ -0,0 +1,26 @@
+package restaurante.team3.Giacobello.tablets.controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import restaurante.team3.Giacobello.tablets.dtos.TabletDTOResponse;
+import restaurante.team3.Giacobello.tablets.service.TabletService;
+
+@RestController 
+@RequestMapping("${api-endpoint}/tablets")
+public class TabletController {
+    private final TabletService tabletService;
+
+    public TabletController(TabletService tabletService) {
+        this.tabletService = tabletService;
+    }
+
+    @GetMapping 
+    public ResponseEntity<List<TabletDTOResponse>> findAll() {
+        return ResponseEntity.ok(tabletService.findAll());
+    }    
+}
