e3b1f558247f6c0df1ebb931e96c997232234033
Rubén Campal <ruben.campal.lopez1996@gmail.com>
2026-09-16 12:20:29 +0200

    feat: add findById method to ProductController and update ProductMapper

--- src/main/java/restaurante/team3/Giacobello/product/controller/ProductController.java
+++ src/main/java/restaurante/team3/Giacobello/product/controller/ProductController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +24,10 @@ public class ProductController {
     @GetMapping 
     public ResponseEntity<List<ProductDTOResponse>> findAll() {
         return ResponseEntity.ok(productService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductDTOResponse> findById(@PathVariable Integer id) {
+        return ResponseEntity.ok(productService.findById(id));
     }
 }
