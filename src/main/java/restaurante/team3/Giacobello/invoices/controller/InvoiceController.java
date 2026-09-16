381ef1fb0ad21e0608689ea3aa31b2d342dfe13d
Ekaterina Zotova <139338863+zookatt@users.noreply.github.com>
2026-09-16 14:03:43 +0200

    feat: add GET order invoice endpoint

---
+++ src/main/java/restaurante/team3/Giacobello/invoices/controller/InvoiceController.java
@@ -0,0 +1,28 @@
+package restaurante.team3.Giacobello.invoices.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import restaurante.team3.Giacobello.invoices.dto.InvoiceDTOResponse;
+import restaurante.team3.Giacobello.invoices.service.InvoiceService;
+
+@RestController
+@RequestMapping("${api-endpoint}/orders")
+public class InvoiceController {
+
+    private final InvoiceService invoiceService;
+
+    public InvoiceController(InvoiceService invoiceService) {
+        this.invoiceService = invoiceService;
+    }
+
+    @GetMapping("/{id}/invoice")
+    public ResponseEntity<InvoiceDTOResponse> findByOrderId(
+            @PathVariable("id") Integer orderId) {
+        return ResponseEntity.ok(
+                invoiceService.findByOrderId(orderId));
+    }
+}
