bb37922b74cdfe2cc2451b4831d0c99d32915ff0
Ekaterina Zotova <139338863+zookatt@users.noreply.github.com>
2026-09-16 14:23:35 +0200

    feat: allow public access to invoice queries

--- src/main/java/restaurante/team3/Giacobello/config/SecurityConfig.java
+++ src/main/java/restaurante/team3/Giacobello/config/SecurityConfig.java
@@ -25,17 +25,19 @@ public class SecurityConfig {
                     .requestMatchers("/images/**", "/error").permitAll()
                     .requestMatchers(HttpMethod.GET, apiEndpoint + "/products").permitAll()
                     .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
-                    .requestMatchers(HttpMethod.GET, apiEndpoint + "/orders", apiEndpoint + "/orders/*")
-                    .permitAll()
-                        .requestMatchers(HttpMethod.GET, apiEndpoint + "/tablets").permitAll()
-                        .requestMatchers(HttpMethod.GET, apiEndpoint + "/tablets/*").permitAll()
+                    .requestMatchers(HttpMethod.GET, apiEndpoint + "/orders", apiEndpoint + "/orders/*").permitAll()
+                    .requestMatchers(HttpMethod.GET, apiEndpoint + "/tablets").permitAll()
+                    .requestMatchers(HttpMethod.GET, apiEndpoint + "/tablets/*").permitAll()
                     .requestMatchers(HttpMethod.GET, apiEndpoint + "/categories").permitAll()
-
-                        .requestMatchers(HttpMethod.GET, apiEndpoint + "/categories/*").permitAll();
+                    .requestMatchers(HttpMethod.GET, apiEndpoint + "/categories/*").permitAll()
+                    .requestMatchers(HttpMethod.GET, apiEndpoint + "/invoices").permitAll();
             if (local) {
                 auth.requestMatchers(
                         HttpMethod.POST,
                         apiEndpoint + "/orders").permitAll();
+                auth.requestMatchers(
+                        HttpMethod.GET,
+                        apiEndpoint + "/orders/*/invoice").permitAll();
             }
             auth.anyRequest().authenticated();
         });
