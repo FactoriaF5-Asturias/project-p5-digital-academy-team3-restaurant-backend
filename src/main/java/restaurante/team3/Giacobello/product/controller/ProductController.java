9953291920c080e831239b29c03b53020ed8bd88
Iulian Timofei <iuliantim21@gmail.com>
2026-09-16 12:32:56 +0200

    feat: add PUT /products/{id} endpoint

--- src/main/java/restaurante/team3/Giacobello/product/controller/ProductController.java
+++ src/main/java/restaurante/team3/Giacobello/product/controller/ProductController.java
@@ -7,7 +7,9 @@ import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,5 +35,12 @@ public class ProductController {
     @PostMapping
     public ResponseEntity<ProductDTOResponse> create(@Valid @RequestBody ProductDTORequest request) {
         return ResponseEntity.status(HttpStatus.CREATED).body(productService.create(request));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ProductDTOResponse> update(
+            @PathVariable Integer id,
+            @Valid @RequestBody ProductDTORequest request) {
+        return ResponseEntity.ok(productService.update(id, request));
     }
 }
