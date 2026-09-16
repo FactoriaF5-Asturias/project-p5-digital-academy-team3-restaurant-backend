71ddaf8df3b932c0581e193d1060c35d02f95230
Iulian Timofei <iuliantim21@gmail.com>
2026-09-16 12:48:33 +0200

    feat: add query to list only active products

--- src/main/java/restaurante/team3/Giacobello/product/repository/ProductRepository.java
+++ src/main/java/restaurante/team3/Giacobello/product/repository/ProductRepository.java
@@ -1,5 +1,8 @@
 package restaurante.team3.Giacobello.product.repository;
 
+import java.util.List;
+
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import restaurante.team3.Giacobello.product.entity.ProductEntity;
@@ -11,4 +14,6 @@ public interface ProductRepository extends JpaRepository<ProductEntity, Integer>
     boolean existsByName(String name);
 
     boolean existsByNameAndIdNot(String name, Integer id);
+
+    List<ProductEntity> findAllByStatusTrue(Sort sort);
 }
