c82098cfe27994c0300641ef9b87bc21fe961542
Iulian Timofei <iuliantim21@gmail.com>
2026-09-24 19:06:46 +0200

    test: enable MockMvc in IntegrationTest base class

--- src/test/java/restaurante/team3/Giacobello/infrastructure/IntegrationTest.java
+++ src/test/java/restaurante/team3/Giacobello/infrastructure/IntegrationTest.java
@@ -1,9 +1,11 @@
 package restaurante.team3.Giacobello.infrastructure;
 
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
+@AutoConfigureMockMvc
 @SpringBootTest
 @ActiveProfiles("test")
 @Import(TestContainers.class)
