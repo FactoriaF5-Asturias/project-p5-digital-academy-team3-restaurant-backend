35618aabe379bbae5b82e511a5118d9e9344ab8a
Iulian Timofei <iuliantim21@gmail.com>
2026-09-24 20:21:55 +0200

    test: verify products sorting and response fields

--- src/test/java/restaurante/team3/Giacobello/product/controller/ProductControllerIntegrationTest.java
+++ src/test/java/restaurante/team3/Giacobello/product/controller/ProductControllerIntegrationTest.java
@@ -22,4 +22,28 @@ class ProductControllerIntegrationTest extends IntegrationTest {
                 .andExpect(jsonPath("$").isArray())
                 .andExpect(jsonPath("$").isNotEmpty());
     }
+
+    @Test
+    void findAllReturnsProductsSortedByName() throws Exception {
+        mockMvc.perform(get("/api/v1/products"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].name").value("Acqua di localhost"))
+                .andExpect(jsonPath("$[1].name").value("AI a la Carte"))
+                .andExpect(jsonPath("$[2].name").value("Arancini Gitilini"));
+
+    }
+
+    @Test
+    void findAllReturnsAllFieldsOfProduct() throws Exception {
+        mockMvc.perform(get("/api/v1/products"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].id").value(10))
+                .andExpect(jsonPath("$[0].name").value("Acqua di localhost"))
+                .andExpect(jsonPath("$[0].description")
+                        .value("Lágrimas acumuladas de alumnos de Giaco que no pudieron terminar los ejercicios"))
+                .andExpect(jsonPath("$[0].category").value("Bebidas"))
+                .andExpect(jsonPath("$[0].price").value(60.0))
+                .andExpect(jsonPath("$[0].status").value(true))
+                .andExpect(jsonPath("$[0].imageUrl").value("/images/products/Acqua_di_localhost.jpg"));
+    }
 }
